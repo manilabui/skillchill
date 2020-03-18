5cec206479e05094b4ebeee2af50d317837b8166
Manila Bui <manila.bui@gmail.com>
2020-03-18 14:35:00 -0500

    created react app + installed react router dom

---
+++ src/modules/apiManager.js
@@ -0,0 +1,32 @@
+const remoteURL = 'http://localhost:8000'
+
+export const getItem = (entity, id) =>
+  fetch(`${remoteURL}/${entity}/${id}`).then(result => result.json())
+
+export const getAll = entity => fetch(`${remoteURL}/${entity}`).then(result => result.json())
+
+export const deleteItem = (entity, id) => {
+  return fetch(`${remoteURL}/${entity}/${id}`, {
+    method: 'DELETE',
+  }).then(r => r.json())
+}
+
+export const postItem = (entity, item) => {
+  return fetch(`${remoteURL}/${entity}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(item),
+  }).then(r => r.json())
+}
+
+export const patchItem = (entity, item) => {
+  return fetch(`${remoteURL}/${entity}/${item.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(item),
+  }).then(r => r.json())
+}
