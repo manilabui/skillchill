0da6369643aeb87c6da2fab1bed621ac6c1a971a
Manila Bui <manila.bui@gmail.com>
2020-03-22 02:35:46 -0500

    one item showing up properly in feed

--- src/components/newsfeed/Post.js
+++ src/components/newsfeed/Post.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import { useSwipeable, Swipeable } from 'react-swipeable'
 import { getAll } from '../../modules/apiManager'
-import PostPage from './PostPage'
 
-export default ({ id, skillager, skill }) => {
+export default ({ id, skillager, skill, post_type }) => {
 	const { name, avatar, created_at } = skill
 	const skill_id = skill.id
 	const [currPostPages, setPages] = useState([])
+	const [currPageNum, setPageNum] = useState(0)
+	const [currPageContent, setContent] = useState('')
+	const [currPageCaption, setCaption] = useState('')
 
-	//fetch all post pages related to current post
 	const getCurrPostPages = () => {
 		getAll(`postpages?post=${id}`)
-			.then(pages => setPages(pages))
+			.then(pages => {
+				if (pages.length) {
+					const { content, caption } = pages[0]
+					setPages(pages)
+					setContent(content)
+					setCaption(caption)
+				}
+			})
 	}
 
 	useEffect(getCurrPostPages, [])
@@ -21,18 +30,39 @@ export default ({ id, skillager, skill }) => {
 	// 	)
 	// })
 
+	const handlePostPageChange = () => {
+		console.log(currPageContent)
+	}
+
+	const currPostElem = post_type === 'Video' ? 
+		<video autoPlay 
+			className='w-100' 
+			onClick={handlePostPageChange} 
+			src={currPageContent}
+			type="video/mp4"
+		/> :
+		<img 
+			src={currPageContent} alt="Post"
+			className='w-100'
+		/>
+
 	return (
 		<article>
 				<div className='mb1 dib pa2 pt3 inline-flex items-center'>
 				  <img
 				      src={avatar} alt="avatar"
-				      className="br-100 h2 w2 dib" />
+				      className="br-100 h1 w1 dib" />
 				 	<span className='pl2 f6 fw6 dib'>{name}</span>
 				</div>
-				<video autoPlay className='w-100'>
-				  <source src={props.content} type="video/mp4" />
-				</video>
-				<h4 className='ph2 f6 fw3'>{props.caption}</h4>
+				<div>
+				<video autoPlay 
+					className='w-100' 
+					onClick={handlePostPageChange} 
+					src={currPageContent}
+					type="video/mp4"
+				/>
+				</div>
+				<h4 className='ph2 f7 fw3'>{currPageCaption}</h4>
 		</article>
 	)
 }
