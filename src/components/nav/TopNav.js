3dcf5faf9645fefdfff13b63b917c1f0b67668d3
Manila Bui <manila.bui@gmail.com>
2020-03-26 01:14:17 -0500

    note on topnav for logout

--- src/components/nav/TopNav.js
+++ src/components/nav/TopNav.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default () => {
 	return (
-			<nav className='fixed tc w-100 bg-light-yellow pv2'>
-				<h1 className='dib f4 fl pl3 fw7'>Skillchill</h1>
-				<Link to='/search/skills'><h1 className='dib f4 fw3'>Search</h1></Link>
-			</nav>
-	)
-}
+		<nav className="fixed tc w-100 bg-light-yellow pv2">
+			<h1 className="dib f4 fl pl3 fw7" onClick=>Skillchill</h1>
+			<Link to="/search/skills">
+				<h1 className="dib f4 fw3">Search</h1>
+			</Link>
+		</nav>
+	);
+};
